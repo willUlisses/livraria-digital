817fd8db63ab264b03d218651c62bf008bd75540
willUlisses <williamcavalcante06@gmail.com>
2025-11-05 13:18:03 -0300

    feat: adiciona inserção e remoção de livros

--- services/livro_service.py
+++ services/livro_service.py
@@ -2,10 +2,38 @@ from config.connection import criar_conexao
 
 ## inserir um livro na tabela
 def inserir_livro(titulo, data_lancamento, id_editora):
-    pass
+    conn = criar_conexao()
+    try:
+        cursor = conn.cursor()
+        sql = "INSERT INTO livros (titulo, data_lancamento, id_editora) VALUES (%s, %s)"
+        cursor.execute(sql, (titulo, data_lancamento, id_editora)) #necessário adicionar verificação da existência da editora
+        conn.commit()
+        print("Livro adicionado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao inserir livro: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 ## retirar um livro da tabela pelo id
+def remover_livro(id_livro):
+    conn = criar_conexao()
+    try:
+        cursor = conn.cursor()
+        sql = "DELETE FROM livros WHERE id_livro = %s"
+        cursor.execute(sql, (id_livro))
+        conn.commit()
+        print("Livro removido com sucesso!")
+    except Exception as e:
+        print(f"Erro ao remover livro: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 ## alterar algo em um livro
+def alterar_livro(id_livro):
+    pass
 
 ## listar livros do user logado
+def listar_livros():
+    pass
