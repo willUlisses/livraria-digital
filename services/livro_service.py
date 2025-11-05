998d0f5b4789f59536b5896be53ad190b6fb55be
willUlisses <williamcavalcante06@gmail.com>
2025-11-05 12:24:43 -0300

    Esquematização das funcoes em livro_service

--- services/livro_service.py
+++ services/livro_service.py
@@ -1,2 +1,11 @@
 from config.connection import criar_conexao
 
+## inserir um livro na tabela
+def inserir_livro(titulo, data_lancamento, id_editora):
+    pass
+
+## retirar um livro da tabela pelo id
+
+## alterar algo em um livro
+
+## listar livros do user logado
