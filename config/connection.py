494ab313e67f1287d31c761963fd299f6c0d92f5
willUlisses <williamcavalcante06@gmail.com>
2025-11-05 12:22:39 -0300

    First Commit!

---
+++ config/connection.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+def criar_conexao():
+    try:
+        conn = psycopg2.connect(
+            dbname="livrariaCesmac_db",
+            user="postgres",
+            password="1nvalidDB",
+            host="localhost",
+            port="5432"
+        )
+        print("Conexão Realizada com sucesso")
+        return conn
+    except Exception as e:
+        print(f"Erro ao conectar com o banco: {e}")
+        return None
---
+++ services/livro_service.py
@@ -0,0 +1,2 @@
+from config.connection import criar_conexao
+
