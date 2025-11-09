ba9512c53089840358beaa5d4fd41546722a20ea
willUlisses <williamcavalcante06@gmail.com>
2025-11-09 00:24:55 -0300

    feat: novas libs os e time adicionadas para manipulação dos paineis no terminal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from views.user_view import *
 from pwinput import pwinput
 from services.cliente_service import *
+import os
+import time
+
+user_logged = None
 
 while True:
     login_user_panel()
@@ -14,15 +18,24 @@ while True:
         senha = pwinput("Senha: ")
         telefone = input("Telefone: ")
         ja_existe = valida_registro(email)
-
         if ja_existe:
             print(f"\nO email {email} já está cadastrado\n")
             continue
         registrar(email, senha, telefone)
+        os.system("cls")
+        print("Agora faça o login com suas credenciais")
+        time.sleep(3)
+        os.system("cls")
     elif login_option == 2:
-        pass
+        print("Informe seus dados\n")
+        email = input("Email: ")
+        senha = pwinput("Senha: ")
+        user_logged = login(email, senha)
+        break
     elif login_option == 3:
+        print("Encerrando o sistema...")
         break
     else:
         print("Opção inválida, tente novamente.")
         continue
+
