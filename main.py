cba692902e865c9657b9d833e722d0b547260b2e
willUlisses <williamcavalcante06@gmail.com>
2025-11-09 17:31:41 -0300

    Construindo exibição do sistema no terminal

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from views.user_view import *
 from pwinput import pwinput
 from services.cliente_service import *
 import os
+import sys
 import time
 
 user_logged = None
@@ -13,6 +14,7 @@ while True:
     except ValueError:
         print("Você pode informar as opções apenas com números.")
     if login_option == 1:
+        os.system("cls")
         print("Informe seus dados\n")
         email = input("Email: ")
         senha = pwinput("Senha: ")
@@ -27,15 +29,26 @@ while True:
         time.sleep(3)
         os.system("cls")
     elif login_option == 2:
+        os.system("cls")
         print("Informe seus dados\n")
         email = input("Email: ")
         senha = pwinput("Senha: ")
         user_logged = login(email, senha)
+        os.system("cls")
         break
     elif login_option == 3:
         print("Encerrando o sistema...")
+        time.sleep(3)
+        sys.exit(0)
         break
     else:
         print("Opção inválida, tente novamente.")
         continue
+
+while True:
+    if user_logged[0] == 1: ##para o caso de ser admin
+        admin_option_panel()
+    else: #para o caso de ser um usuário normal
+        user_options_panel()
+        
 
