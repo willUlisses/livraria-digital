28b733e20aa051e106cd0b36242ec867eff55f3e
willUlisses <williamcavalcante06@gmail.com>
2025-11-14 23:19:08 -0300

    add: incremento do painel de continuar compras do usuário

--- views/user_view.py
+++ views/user_view.py
@@ -8,12 +8,16 @@ def user_options_panel():
     print("1- Comprar um Livro\n2 - Buscar Livro\n3 - Listar Seus Livros")
 
 def admin_option_panel():
-    print("BEM VINDO!\n")
-    print("1 - Livros\n2 - Editoras\n3 - Autores\n4 - Vendas") #considerar opção para atuar com clientes
+    print("BEM VINDO!\nEscolha o que deseja modificar:\n")
+    print("1 - Livros\n2 - Editoras\n3 - Autores\n4 - Vendas\n5 - Sair do Sistema") #considerar opção para atuar com clientes
 
 def admin_livros_panel():
     print("O que você deseja modificar nos livros?\n")
     print("1 - Adicionar um livro\n2 - Remover um livro\n3 - Modificar um livro\n4 - Buscar Livros\n5 - Voltar ao inicio")
+
+def user_compras_panel():
+    print("Informe o que deseja fazer:\n")
+    print("1 - Continuar comprando\n2 - Encerrar Compras")
 
 def admin_editoras_panel():
     print("O que você deseja modificar nas editoras?\n")
