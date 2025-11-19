b35e7d0095264798b375d0f5654f2c7e87181ff9
willUlisses <williamcavalcante06@gmail.com>
2025-11-19 01:51:54 -0300

    finalizando lógica administrativa para crud de editoras

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from services.vendas_service import *
 import os
 import sys
 import time
-from datetime import date, datetime
+from datetime import date
+from psycopg2.errors import ForeignKeyViolation
+
+
 
 user_logged = None
 
@@ -70,7 +73,7 @@ while True:
                 os.system("cls")
                 print("Você deve digitar apenas números.")
                 continue
-            if admin_option == 1: # ----------- Livros
+            if admin_option == 1: # ----------------------------------------------------- Livros
                 os.system("cls")
                 while True:
                     os.system("cls")
@@ -85,7 +88,7 @@ while True:
 
                     match(livros_option):
                         case 1:
-                            pass #Adicionar livro
+                            pass #------------------> Adicionar livro
                         case 2:
                             os.system("cls")
                             print("Aqui está a lista dos livros disponíveis no estoque:\n")
@@ -131,7 +134,7 @@ while True:
                             os.system("cls")
                             print("Digite uma opção válida.")
                             continue
-            elif admin_option == 2: # ----------- editoras
+            elif admin_option == 2: # ---------------------------------------------------- editoras
                 while True:
                     os.system("cls")
                     admin_editoras_panel()
@@ -156,11 +159,80 @@ while True:
                             print("Aqui estão as editoras existentes no sistema:\n")
                             editoras_existentes = listar_editoras()
                             time.sleep(0.7)
-                            
+
+                            for editora in editoras_existentes:
+                                time.sleep(0.7)
+                                print("------------------")
+                                print(f"{editora[0]} -> {editora[1]}")
+                                print("------------------")
+
+                            try:
+                                id_editora_escolhida = int(input("Informe o id da editora a ser removida ou digite 0 para sair:\n"))
+                            except ValueError:
+                                os.system("cls")
+                                print("Você deve digitar apenas valores númericos")
+                                continue
+                            if id_editora_escolhida != 0:
+                                try:
+                                    remover_editora_por_id(id_editora_escolhida)
+                                    print("A editora foi removida com sucesso do sistema!\n")
+                                    input("Pressione qualquer tecla para continuar...")
+                                except ForeignKeyViolation:
+                                    os.system("cls")
+                                    print("Não é possível excluir uma editora associada à um ou mais livros, tente excluir o livro primeiro!")
+                                    input("\nPressione qualquer tecla para voltar ao menu...")
+                                    continue
+                            else:
+                                os.system("cls")
+                                print("Voltando ao menu principal...")
+                                time.sleep(1.5)
+                                break
                         case 3: 
-                            pass #modificar 
+                            os.system("cls")
+                            editoras_no_sistema = listar_editoras()
+
+                            if editoras_no_sistema:
+                                print("Essas são as editoras presentes no sistema:\n")
+
+                                for editora in editoras_no_sistema:
+                                     time.sleep(0.7)
+                                     print("------------------")
+                                     print(f"Id: {editora[0]} -> Nome: {editora[1]}")
+                                     print("------------------")
+                                
+                                try:
+                                    id_editora_para_editar = int(input("Digite o id da editora que deseja modificar ou digite 0 para voltar ao menu: \n"))
+                                    os.system("cls")
+                                    novo_nome = input("Informe o nome que deseja por nessa editora: ")
+                                    alterar_nome_editora(novo_nome, id_editora_para_editar)
+                                    print("Editora modificada com sucesso!")
+                                    input("\nPressione qualquer tecla para continuar...")
+                                except ValueError:
+                                    os.system("cls")
+                                    print("Informe apenas valores numéricos.")
+                                    input("Pressione qualquer tecla para voltar ao menu...")
+                                    continue
                         case 4:
-                            pass #buscar por nome
+                            os.system("cls")
+                            print("Informe o nome da editora que deseja encontar:")
+                            nome_de_busca = input("Pesquisar: ")
+                            resultados = buscar_editora_por_nome(nome_de_busca)
+
+                            if resultados:
+                                os.system("cls")
+                                print("Possíveis resultados:\n")
+                                for editora in resultados:
+                                    time.sleep(0.7)
+                                    print("------------------")
+                                    print(f"Nome: {editora[0]} -> Cidade: {editora[1]}")
+                                    print("------------------")
+                                
+                                input("\nPressione qualquer tecla para voltar ao menu...")
+                            else:
+                                os.system("cls")
+                                print("Não foi possível encontrar nenhum resultado compatível com sua busca")
+                                input("Pressione qualquer tecla para voltar ao menu...")
+                                continue
                         case 0:
                             os.system("cls")
                             print("Voltando para o menu inicial...")
@@ -170,7 +242,7 @@ while True:
                             os.system("cls")
                             print("Digite uma opção válida.")
                             continue
-            elif admin_option == 3: # ----------- Autores
+            elif admin_option == 3: # -------------------------------------------------------------------- Autores
                 while True:
                     os.system("cls")
                     admin_autores_panel()
@@ -200,7 +272,7 @@ while True:
                             os.system("cls")
                             print("Digite uma opção válida.")
                             continue
-            elif admin_option == 4: # ----------- Vendas
+            elif admin_option == 4: # ---------------------------------------------------------------- Vendas
                 while True:
                     os.system("cls")
                     admin_vendas_panel()
